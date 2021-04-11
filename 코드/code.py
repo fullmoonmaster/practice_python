ac2428d26d5928776fe99b19fdebb6ddba038863
fullmoonmaster <82299558+fullmoonmaster@users.noreply.github.com>
2021-04-11 12:44:56 +0900

    Add files via upload

---
+++ 코드/code.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
+
+7%3
+
+
+# In[10]:
+
+
+7//3
+
+
+# In[11]:
+
+
+a = 1
+b = 2
+c = a+b
+print(c)
+
+
+# In[12]:
+
+
+food = 'apple'
+
+
+# In[13]:
+
+
+print(food)
+
+
+# In[14]:
+
+
+a = "Python's favorite food is perl"
+
+
+# In[15]:
+
+
+a
+
+
+# In[16]:
+
+
+food = "딸기"
+a = "수박"
+food + a
+
+
+# In[ ]:
+
+
+
+
